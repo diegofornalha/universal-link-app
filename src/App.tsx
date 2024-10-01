da7e3159d5d0623b15e292d932453ddd273e5906
Diego Fornalha <diegofornalha@gmail.com>
2024-10-01 16:09:03 -0300

    doterra

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './App.css'; // Adicione os estilos aqui ou use Styled Components
 
-function App() {
+const App: React.FC = () => {
+  // Função para detectar o dispositivo e redirecionar automaticamente
+  const detectDeviceAndRedirect = () => {
+    const userAgent = navigator.userAgent || navigator.vendor;
+    const appUrlScheme = "weixin://dl/business/?t=jw5RP9P4GVf";
+    const appStoreUrl = "https://apps.apple.com/pt/app/phiz-chat/id6447375837";
+    const googlePlayUrl = "https://play.google.com/store/apps/details?id=live.phiz.app2";
+    const fallbackUrl = "https://phiz.com.br/download";
+
+    if (/android/i.test(userAgent)) {
+      // Redireciona para o app ou Google Play no Android
+      window.location.href = appUrlScheme;
+      setTimeout(() => {
+        window.location.href = googlePlayUrl;
+      }, 1500);
+    } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+      // Redireciona para o app ou App Store no iOS
+      window.location.href = appUrlScheme;
+      setTimeout(() => {
+        window.location.href = appStoreUrl;
+      }, 1500);
+    } else {
+      // Redireciona para o fallback no desktop
+      window.location.href = fallbackUrl;
+    }
+  };
+
+  useEffect(() => {
+    // Executa o redirecionamento assim que o componente montar
+    detectDeviceAndRedirect();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="main">
+        <img
+          src="https://app.phiz.chat/oss/view/2024101ac765480-350f-4576-890d-bd83eb3a518e.jpg"
+          className="screenshot"
+          alt="Doterra"
+        />
+      </div>
+      <div className="popup">
+        <div className="popup__header">
+          <img
+            className="popup__logo"
+            src="https://app.phiz.chat/oss/view/2024917455bbc15-4bc6-4989-b562-6c2d9e29d52a.jpg"
+            alt="Doterra Logo"
+          />
+          <span className="popup__name">Doterra</span>
+        </div>
+        <div className="popup__desc"></div>
+        <button className="popup__button">Open in Phiz App</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
